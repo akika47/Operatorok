277b15bddea1b1f62594bad8a135896a21918089
Ákos Ö <akika02531@gmail.com>
2023-05-24 10:21:44 +0200

    4. feladatig kész

--- Program.cs
+++ Program.cs
@@ -6,15 +6,32 @@
 		{
 			List<Operatorok> kifejezesek = new List<Operatorok>();
 			StreamReader sr = new StreamReader("kifejezesek.txt");
-			sr.ReadLine();
 			while (!sr.EndOfStream)
 			{
 				var mezok = sr.ReadLine().Split(' ');
-				kifejezesek beolvas = new kifejezesek(int.Parse(mezok[0]), mezok[1][0], int.Parse(mezok[2]));
+				Operatorok beolvas = new Operatorok(int.Parse(mezok[0]), mezok[1], int.Parse(mezok[2]));
 				kifejezesek.Add(beolvas);
 			}
 			sr.Close();
-			Console.WriteLine("Hello World!");
+
+
+			//2.feladat
+			Console.WriteLine($"2. feladat: Kifejezések száma: {kifejezesek.Count}");
+
+			//3.feladat
+			Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {kifejezesek.Count(x => x.Op == "mod")}");
+
+			//4.feladat
+			if (kifejezesek.Any(x => x.ElsoSzam % 10 == 0 && x.MasodikSzam % 10 == 0) == true)
+			{
+				Console.WriteLine("4. feladat: Van ilyen kifejezés!");
+			}
+			else { Console.WriteLine("4. feladat: Nincs ilyen kifejezés!"); }
+
+
+
+			
+
 		}
 	}
 }
