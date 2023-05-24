8fa05d124af869455693ea2c9c4cb4fcdc48d102
Ákos Ö <akika02531@gmail.com>
2023-05-24 10:09:56 +0200

    Kész a class és beolvasás

---
+++ Operatorok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operátorok
+{
+	internal class Operatorok
+	{
+		int elsoSzam;
+		string op;
+		int masodikSzam;
+
+		public Operatorok(int elsoSzam, string op, int masodikSzam)
+		{
+			this.elsoSzam = elsoSzam;
+			this.op = op;
+			this.masodikSzam = masodikSzam;
+		}
+
+		public int ElsoSzam { get => elsoSzam;}
+		public string Op { get => op;}
+		public int MasodikSzam { get => masodikSzam;}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@
 	{
 		static void Main(string[] args)
 		{
+			List<Operatorok> kifejezesek = new List<Operatorok>();
+			StreamReader sr = new StreamReader("kifejezesek.txt");
+			sr.ReadLine();
+			while (!sr.EndOfStream)
+			{
+				var mezok = sr.ReadLine().Split(' ');
+				kifejezesek beolvas = new kifejezesek(int.Parse(mezok[0]), mezok[1][0], int.Parse(mezok[2]));
+				kifejezesek.Add(beolvas);
+			}
+			sr.Close();
 			Console.WriteLine("Hello World!");
 		}
 	}
